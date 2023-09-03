79108979389dd5a14d34cbb9f9356bf9ccd4a32a
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-09-03 16:33:16 +0545

    csit index page and frontend fixes

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="mt-auto text-gray-400 text-sm my-4 text-center">
+<footer class="z-50 mt-auto text-gray-400 text-sm my-4 text-center">
     © 2023, Deerwalk Institute of Technology, All rights reserved.
 </footer>
--- resources/views/csit/index.blade.php
+++ resources/views/csit/index.blade.php
@@ -1,4 +1,20 @@
 @extends('layouts.layout')
 @section('content')
     @include('components.sidebar')
+    <div class="xl:ml-40 my-4 xl:mt-0 mb-8">
+        <p class="text-center text-xl text-gray-200 mb-4">Batch of 2011</p>
+        <div class="grid place-items-center md:grid-cols-3 gap-5 md:gap-8 xl:gap-16">
+            @for ($i = 0; $i < 8; $i++)
+                <div class="max-w-xs rounded overflow-hidden bg-[#18212f] border-2 border-[#242c34]">
+                    <img class="w-full aspect-3/2 object-cover" src="{{ asset('images/tej.jpg') }}"
+                        alt="Sunset in the mountains">
+                    <div class="flex items-center justify-between px-3">
+                        <p class="text-xlfont-semibold text-gray-300 text-center p-2">Tej Agrawal</p>
+                        <a href="https://www.linkedin.com" target="blank"><img src="{{ asset('icons/linkedin.svg') }}"
+                                alt="link_to_linkedin" class="h-5 object-contain aspect-3/2"></a>
+                    </div>
+                </div>
+            @endfor
+        </div>
+    </div>
 @endsection

f40d0897553b5c2591340a2b50e76d7e92600ec0
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-09-03 15:21:36 +0545

    added csit page

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,15 +8,16 @@
     <title>Doko</title>
     <link rel="shortcut icon" href="{{ asset('icons/deer_logo.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
     @vite('resources/css/app.css')
 </head>
 
-<body class="bg-[#111827] max-w-7xl mx-auto min-h-screen flex flex-col">
-    @include('components.navbar')
+<body class="bg-[#111827] max-w-7xl mx-auto px-3 min-h-screen flex flex-col">
     @yield('content')
     @include('components.footer')
     <script src="{{ asset('js/particle.js') }}"></script>
     <script src="{{ asset('js/my.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
 </body>
 
 </html>

5fb01230a87084e555b0fb584df4ae5ba387c5d3
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-09-04 22:18:17 +0545

    hidden sidebar and updated frontend

--- resources/views/csit/index.blade.php
+++ resources/views/csit/index.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.layout')
 @section('content')
     @include('components.sidebar')
-    <div class="xl:ml-40 my-4 xl:mt-0 mb-8">
-        <p class="text-center text-xl text-gray-200 mb-4">Batch of 2011</p>
+    <div class="mt-4 mb-16">
+        <p class="text-center text-3xl text-gray-200 mb-8">Batch of 2011</p>
         <div class="grid place-items-center sm:grid-cols-2 md:grid-cols-3 gap-5 md:gap-8 xl:gap-6 2xl:gap-12">
             @for ($i = 0; $i < 8; $i++)
-                <div class="max-w-xs rounded overflow-hidden bg-[#18212f] border-2 border-[#242c34]">
+                <div
+                    class="max-w-xs rounded overflow-hidden bg-[#18212f] border-2 border-[#242c34] hover:shadow hover:shadow-[#ef4444]/30 md:hover:-mt-2">
                     <img class="w-full aspect-3/2 object-cover" src="{{ asset('images/tej.jpg') }}"
                         alt="Sunset in the mountains">
                     <div class="flex items-center justify-between px-3">
